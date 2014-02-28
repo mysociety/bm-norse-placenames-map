e9f571ae52ec30f0c13a8ea44fc514f81521b3b7
Steven Day <steve@mysociety.org>
2014-02-28 12:10:12 +0000

    Limit the map controls that are available
    This makes things more predictable between mobile and desktop views

--- web/js/main.js
+++ web/js/main.js
@@ -189,7 +189,11 @@
             zoom: 6,
             // Centered on "The centre of England" as per:
             // http://en.wikipedia.org/wiki/Centre_points_of_the_United_Kingdom
-            center: new google.maps.LatLng(52.561928, -1.464854)
+            center: new google.maps.LatLng(52.561928, -1.464854),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: false,
+            streetViewControl: false,
+            panControl: false
         };
 
         // Create the map
