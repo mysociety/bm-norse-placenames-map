33e561e9f0034a4fb9f98cfd3bf49baf94272988
Steven Day <steve@mysociety.org>
2014-04-04 10:31:04 +0100

    Condense vikings live info even more

--- web/index.php
+++ web/index.php
@@ -93,26 +93,19 @@
             <% }); %>
             </ul>
             <% if(showNearestCinema) { %>
-                <h3 class="map-marker__cinema-header">Nearest Vikings Live showing:</h3>
-                <h4 class="map-marker__cinema-name">
+                <h3 class="map-marker__cinema-name">
+                    Nearest Vikings Live showing:
                     <% if(place.cinema.cinemaurl !== "") { %>
                         <a href="<%= place.cinema.cinemaurl %>"><%= place.cinema.cinema %>: <%= place.cinema.live %></a>
+                    <% } else if (place.cinema.cinemaemail !== "") { %>
+                        <a href="mailto:<%= place.cinema.cinemaemail %>"><%= place.cinema.cinema %>: <%= place.cinema.live %>
                     <% } else { %>
                         <%= place.cinema.cinema %>: <%= place.cinema.live %>
+                        <% if (place.cinema.phone !== "") { %>
+                            (<a href="tel:<%= place.cinema.phone %>"><%= place.cinema.phone %></a>)
+                        <% } %>
                     <% } %>
-                </h4>
-                <ul class="map-marker__cinema-link">
-                    <li>
-                        <% if(place.cinema.cinemaurl == "" && place.cinema.cinemaemail !== "") { %>
-                            <a href="mailto:<%= place.cinema.cinemaemail %>">Email this cinema</a>
-                        <% } %>
-                    </li>
-                    </li>
-                        <% if(place.cinema.cinemaurl == "" && place.cinema.cinemaemail == "" && place.cinema.phone !== "") { %>
-                            <a href="tel:<%= place.cinema.phone %>">Phone this cinema on: <%= place.cinema.phone %></a>
-                        <% } %>
-                    </li>
-                </ul>
+                </h3>
             <% } %>
             <h3 class="map-marker__social-header">Share this place</h3>
             <ul class="map-marker__social-buttons">
